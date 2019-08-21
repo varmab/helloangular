7b160df055c70ff3c906e7d85fbd7366b89e3efc
Varma Bhupatiraju <varmab@gmail.com>
2019-08-22 03:47:56 +0530

    library

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { AppComponent} from './app.component'
+import { LibraryComponent } from './library.component'
 
 @NgModule({
-  imports: [
-    BrowserModule
+  declarations:[
+    AppComponent,
+    LibraryComponent
   ],
-  bootstrap: [
-    AppComponent
+  bootstrap:[AppComponent],
+  imports:[
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
-  declarations: [
-    AppComponent
+  exports:[
+    LibraryComponent
   ]
 })
-export class AppModule {}
+export class AppModule{
+
+}
---
+++ app/library.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+interface Book {
+    title:string;
+    author:string;
+}
+
+@Component({
+    selector:"books-library",
+    template:`
+        <div>
+            <h1>Books Library</h1>
+            <input type="text" #title/>
+            <input type="text" #author/>
+            <button (click)="addBook(title.value,author.value)">Add Book</button>
+            <table>
+                <tr>
+                    <td>Title</td>
+                    <td>Author</td>
+                </tr>
+                <tr *ngFor="let book of books">
+                    <td>{{ book.title }}</td>
+                    <td>{{ book.author }}</td>
+                </tr>
+            </table>
+        </div>
+    `
+})
+export class LibraryComponent {
+    books:Book[]=[];
+
+    addBook(title:string,author:string){
+        var newBook={
+            title,
+            author
+        }
+
+        this.books.push(newBook)
+    }
+}
